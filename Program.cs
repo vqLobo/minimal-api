c2b2c09ce4a212b139a55777363f9563b2cc9581
Vinicius Lobo Quirino <viniciuslobo.quirino@gmail.com>
2025-09-23 15:40:54 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+app.Run();
